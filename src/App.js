c569250c8825cba9d66fd0c15ba85e2edb7d1e7c
Florent Galon <>
2021-06-22 17:55:12 +0200

    router fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import "./App.css";
-import Sidebar from "./Sidebar";
-import Chat from "./Chat";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="app">
-      <div className="app__body">
-        <Router>
-          <Switch>
-            <Route path="/app">
-              <Sidebar />
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Home screen</h1>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+    <div className='app'>
+      {!user ? (
+        <h1>Login</h1>
+      ) : (
+        <div className='app__body'>
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path='/rooms/:roomId'>
+                <Chat />
+              </Route>
+              <Route path='/'>
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
