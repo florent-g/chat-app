70943cdddae46534dd2b7d35671c9c93ef72db3c
Florent Galon <>
2021-06-24 11:46:55 +0200

    messages

--- src/Chat.js
+++ src/Chat.js
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { Avatar, IconButton } from "@material-ui/core";
+import React, { useEffect, useState } from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
 import {
   SearchOutlined,
   MoreVert,
   AttachFile,
   InsertEmoticon,
-} from "@material-ui/icons";
-import MicIcon from "@material-ui/icons/Mic";
-import "./Chat.css";
-import { useParams } from "react-router-dom";
-import db from "./firebase";
+  MessageSharp,
+} from '@material-ui/icons';
+import MicIcon from '@material-ui/icons/Mic';
+import './Chat.css';
+import { useParams } from 'react-router-dom';
+import db from './firebase';
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase';
 
 function Chat() {
-  const [seed, setSeed] = useState("");
-  const [input, setInput] = useState("");
+  const [seed, setSeed] = useState('');
+  const [input, setInput] = useState('');
   const { roomId } = useParams();
-  const [roomName, setRoomName] = useState("");
+  const [roomName, setRoomName] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -23,27 +28,38 @@ function Chat() {
 
   useEffect(() => {
     if (roomId) {
-      db.collection("rooms")
+      db.collection('rooms')
         .doc(roomId)
         .onSnapshot((snapShot) => setRoomName(snapShot.data().name));
+      db.collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
     }
   }, [roomId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
-    console.log(input);
-    setInput("");
+    db.collection('rooms').doc(roomId).collection('messages').add({
+      message: input,
+      name: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput('');
   };
 
   return (
-    <div className="chat">
-      <div className="chat__header">
+    <div className='chat'>
+      <div className='chat__header'>
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-        <div className="chat__headerInfo">
+        <div className='chat__headerInfo'>
           <h3>{roomName}</h3>
           <p>Last seen at ...</p>
         </div>
-        <div className="chat__headerRight">
+        <div className='chat__headerRight'>
           <IconButton>
             <SearchOutlined />
           </IconButton>
@@ -55,23 +71,31 @@ function Chat() {
           </IconButton>
         </div>
       </div>
-      <div className="chat__body">
-        <p className={`chat__message ${true && "chat__reciever"}`}>
-          <span className="chat__name">Bob</span>
-          hey
-          <span className="chat__timestamp">3:52pm</span>
-        </p>
+      <div className='chat__body'>
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${
+              message.name === user.displayName && 'chat__reciever'
+            }`}
+          >
+            <span className='chat__name'>{message.name}</span>
+            {message.message}
+            <span className='chat__timestamp'>
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
       </div>
-      <div className="chat__footer">
+      <div className='chat__footer'>
         <InsertEmoticon />
         <form>
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            type="text"
-            placeholder="Type a message"
+            type='text'
+            placeholder='Type a message'
           />
-          <button onClick={sendMessage} type="submit">
+          <button onClick={sendMessage} type='submit'>
             Send message
           </button>
         </form>
