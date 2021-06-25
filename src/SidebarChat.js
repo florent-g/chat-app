613b2f3b36be918ed27561e5f514725c76d06190
Florent Galon <>
2021-06-25 11:27:28 +0200

    end

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,20 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Avatar } from "@material-ui/core";
-import "./SidebarChat.css";
-import db from "./firebase";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Avatar } from '@material-ui/core';
+import './SidebarChat.css';
+import db from './firebase';
+import { Link } from 'react-router-dom';
 
 function SidebarChat({ id, name, addNewChat }) {
-  const [seed, setSeed] = useState("");
+  const [seed, setSeed] = useState('');
+  const [messages, setMessages] = useState('');
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
+  useEffect(() => {
+    if (id) {
+      db.collection('rooms')
+        .doc(id)
+        .collection('messages')
+        .orderBy('timestamp', 'desc')
+        .onSnapshot((snapshot) => {
+          setMessages(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+  }, [id]);
+
   const createChat = () => {
-    const roomName = prompt("Please enter name for chat");
+    const roomName = prompt('Please enter name for chat');
     if (roomName) {
-      db.collection("rooms").add({
+      db.collection('rooms').add({
         name: roomName,
       });
     }
@@ -22,16 +35,16 @@ function SidebarChat({ id, name, addNewChat }) {
 
   return !addNewChat ? (
     <Link to={`/rooms/${id}`}>
-      <div className="sidebarChat">
+      <div className='sidebarChat'>
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-        <div className="sidebarChat__info">
+        <div className='sidebarChat__info'>
           <h2>{name}</h2>
-          <p>Last message...</p>
+          <p>{messages[0]?.message}</p>
         </div>
       </div>
     </Link>
   ) : (
-    <div onClick={createChat} className="sidebarChat">
+    <div onClick={createChat} className='sidebarChat'>
       <h2>Add new chat</h2>
     </div>
   );
